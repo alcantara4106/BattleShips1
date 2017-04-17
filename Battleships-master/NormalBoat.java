d84d1bd492139ec6fa7e11998ad38ab901b55997
Ethan Xavier Alcantara <Alcantara94106>
2017-04-17 11:15:41 -0700

    added files

---
+++ Battleships-master/NormalBoat.java
@@ -0,0 +1,15 @@
+import greenfoot.*;
+/**
+ * Write a description of class NormalBoat here.
+ * 
+ * @author Ethan Alcantara 
+ * @version v1.0 4/17/17
+ */
+public class NormalBoat extends Ship
+{
+    public void act(){
+        if(Greenfoot.isKeyDown("right")){setLocation(getX() + 5, getY());}
+        if(Greenfoot.isKeyDown("left")){setLocation(getX() - 5, getY());}
+        if(Greenfoot.isKeyDown("space")){setLocation(getX() + 5, getY());}
+    }
+}
